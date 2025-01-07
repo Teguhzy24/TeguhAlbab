8d308cbe11b6b4a963ce201bbaf29ff764da0579
Teguhzy24 <teguhalbab041@gmail.com>
2025-01-07 08:33:59 +0700

    sudah ngerjain portofolio

---
+++ main.js
@@ -0,0 +1,11 @@
+// Toggle & Responsive Navigation
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navlist = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+    navlist.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+    });  
+};
+navSlide();
